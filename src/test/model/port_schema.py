296072ca2120b2eaf38f23fbd16d5a41c52afffd
Hans Kammler <coolmacmaniac@gmail.com>
2024-06-28 15:29:01 +0530

    Test conf
    Making date-range optional, if dates are not provided all data is loaded w.r.t. the dates in benchmark.

--- src/test/model/port_schema.py
+++ src/test/model/port_schema.py
@@ -1,12 +1,12 @@
-from typing import List
+from typing import List, Optional
 from pydantic import BaseModel, StrictStr
 
 
 class Portfolio(BaseModel):
     description: StrictStr
     symbols: List[StrictStr]
-    start_date: StrictStr
-    end_date: StrictStr
+    start_date: Optional[StrictStr] = None
+    end_date: Optional[StrictStr] = None
     weights: List[float]
     initial_value: int
 
--- src/util/data.py
+++ src/util/data.py
@@ -15,19 +15,41 @@ def symbol_to_path(symbol, base_dir=None):
     return os.path.join(market_data_dir, '{}.csv'.format(str(symbol)))
 
 # ---------------------------------------------------------------------------
-def get_data(symbols, dates, addSPY=True, colname='Adj Close'):
+def get_price_data(symbol, colname='Adj Close'):
+    df = pd.read_csv(symbol_to_path(symbol), index_col='Date',
+                     parse_dates=True, usecols=['Date', colname], na_values=['nan'])
+    df = df.rename(columns={colname: symbol})
+    return df
+
+# ---------------------------------------------------------------------------
+def get_data(symbols, dates=None, addSPY=True, colname='Adj Close'):
     """Read stock data (adjusted close) for given symbols from CSV files."""
+    benchmark = 'SPY'
     df = pd.DataFrame(index=dates)
-    if addSPY and 'SPY' not in symbols:  # add SPY for reference, if absent
-        symbols = ['SPY'] + symbols
+    #
+    if addSPY or benchmark in symbols:
+        df_temp = get_price_data(benchmark, colname)
+        if dates is not None:
+            df = df.join(df_temp)
+        else:
+            # dates should be in the chronological order
+            df = df_temp.sort_index()
+    #
+    if benchmark in symbols:
+        # SPY is already handled, remove it from symbols if it is there
+        symbols.remove(benchmark)
     #
     for symbol in symbols:
-        df_temp = pd.read_csv(symbol_to_path(symbol), index_col='Date',
-                parse_dates=True, usecols=['Date', colname], na_values=['nan'])
-        df_temp = df_temp.rename(columns={colname: symbol})
+        df_temp = get_price_data(symbol, colname)
         df = df.join(df_temp)
-        if symbol == 'SPY':  # drop dates SPY did not trade
-            df = df.dropna(subset=['SPY'])
+    #
+    if benchmark in df.columns:
+        # if benchmark is added, drop the dates when it did not trade
+        df = df.dropna(subset=[benchmark])
+    else:
+        # only assets are there
+        # remove those dates when price is not there for all symbols
+        df = df.dropna(how='all')
     #
     return df
 
